c626a13f999a988adc0f47af3a878aabf4ac5783
Joy.Wu <Joy.Wu@PRCSGI1655L.ad.infosys.com>
2016-04-07 10:47:02 +0800

    111

---
+++ BBS/WebContent/js/comme/formvalues.js
@@ -0,0 +1,25 @@
+﻿(function ($) {
+    $.fn.getjsonvalues = function () {
+        var retVal = new Array();
+        $(this).find(":input").each(function () {
+            if ($(this).attr("type") == "text" || $(this).attr("type") == "hidden" || $(this).attr("type") == "file")
+            {
+                var name = $(this).attr("name");
+                var value = $.trim($(this).val());
+                retVal[name] = value;
+            }
+            else if ($(this).attr("type") == "radio") {
+                var name = $(this).attr("name");
+                var value = $.trim($(this).val());
+                retVal[name] = $(this).prop("checked") ? value : "";
+            }
+            else if ($(this)[0].tagName.toLowerCase() == "select") {
+                var name = $(this).attr("name");
+                var value = $.trim($(this).val());
+                retVal[name] = value;
+            }
+        });
+        console.log(retVal);
+        return retVal;
+    };
+})(jQuery);
