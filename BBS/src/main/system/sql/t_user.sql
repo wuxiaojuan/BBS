1712243f4f47b9a655d075748ae3c1f106de2baa
Joy.Wu <Joy.Wu@PRCSGI1655L.ad.infosys.com>
2016-04-20 10:50:15 +0800

    3

--- BBS/src/main/system/sql/t_user.sql
+++ BBS/src/main/system/sql/t_user.sql
@@ -25,10 +25,12 @@ CREATE TABLE `t_user` (
   `password` varchar(50) NOT NULL,
   `qq` varchar(50) NOT NULL,
   `type` int(11) NOT NULL DEFAULT '1' COMMENT '1为普通用户，2为管理员',
+  `valid` int(11) NOT NULL DEFAULT '1' COMMENT '1:有效 0:无效',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('12', 'wuxiaojuan', '96e79218965eb72c92a549dd5a330112', '12121312', '1');
+INSERT INTO `t_user` VALUES ('12', 'admin', '96e79218965eb72c92a549dd5a330112', '12121312', '1');
+INSERT INTO `t_user` VALUES ('12', 'admin', '96e79218965eb72c92a549dd5a330112', '12121312', '2');
